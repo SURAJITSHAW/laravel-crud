2c1cbb92bc6fcee99966fc6ad384db1af13735ef
Surajit <piklu3043@gmail.com>
2023-12-21 15:10:07 +0530

    Breakdown the html with layouts

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,43 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.base')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel CRUD</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-</head>
-
-<body>
-
-    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active text-white" aria-current="page"
-                            href="{{ route('products.index') }}">Products</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    {{-- Flash Messages --}}
-    @if (session('success'))
-        <div class="alert alert-success">
-            <strong>{{ session('success') }}</strong>
-        </div>
-    @endif
-
-
+@section('main')
     <div class="container">
         <div class="text-end">
             <a class="btn btn-dark mt-2" href="{{ route('products.create') }}">New Product</a>
@@ -60,12 +23,12 @@
                         <td>{{ $loop->index + 1 }}</td>
                         <td>{{ $product->name }}</td>
                         <td>
-                            <img src="{{ asset('uploads/' . $product->image) }}" alt="" width="60px"
-                                height="60px" class="rounded-circle">
+                            <img src="{{ asset('uploads/' . $product->image) }}" alt="" width="60px" height="60px"
+                                class="rounded-circle">
                         </td>
                         <td>
-                            <a href="product/{{$product->id}}/edit" class="btn btn-sm btn-dark ">Edit</a>
-                            <a href="product/{{$product->id}}/delete" class="btn btn-sm btn-danger  ">Delete</a>
+                            <a href="product/{{ $product->id }}/edit" class="btn btn-sm btn-dark ">Edit</a>
+                            <a href="product/{{ $product->id }}/delete" class="btn btn-sm btn-danger  ">Delete</a>
                         </td>
                     </tr>
                 @endforeach
@@ -74,13 +37,4 @@
 
 
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
